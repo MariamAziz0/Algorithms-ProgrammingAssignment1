de94337a2d3af23b34f3cf07611d9ba6913a5fb1
MariamAziz0 <ma27523@gmail.com>
2023-11-07 17:43:04 +0200

    Update Main.java

--- JavaProject/src/Part1/Main.java
+++ JavaProject/src/Part1/Main.java
@@ -3,46 +3,6 @@ package Part1;
 import java.util.Arrays;
 
 public class Main {
-//    public static void main(String[] args) {
-////        int[] array = new int[] {6, 10, 13, 5, 8, 3, 2, 11, 12, 15, 200, 1, 1};
-////        for(int i = 0 ; i < array.length ; i++){
-////            System.out.println(RandomizedDivideANDConquer.randomizedSelection(array, i + 1) + " " +
-////                    DeterministicLinearTimeSelection.linearTimeSelection(array, i + 1) + " " +
-////                    SortingSelection.sortingSelection(array, i + 1));
-////        }
-//
-//        System.out.println("-----------------------------------------------------------------------------");
-//
-//        int[] array2 = HelpingFunctions.generateDistinctRandomArray(10000000, -10000000, 10000000);
-////        for(int i = 0 ; i < array2.length ; i++){
-////            System.out.println(RandomizedDivideANDConquer.randomizedSelection(array2, i + 1) + " " +
-////                    DeterministicLinearTimeSelection.linearTimeSelection(array2, i + 1) + " " +
-////                    SortingSelection.sortingSelection(array2, i + 1));
-////        }
-//        long startTime = System.currentTimeMillis();
-////        for(int i = 0 ; i < array2.length ; i++) {
-////            RandomizedDivideANDConquer.randomizedSelection(array2, i+1);
-////        }
-//        int random = RandomizedDivideANDConquer.randomizedSelection(Arrays.copyOf(array2, array2.length), array2.length / 2);
-//        long endTime = System.currentTimeMillis();
-//        System.out.println("Time taken by random (" + random + "): " + ((endTime - startTime)));
-//
-//        startTime = System.currentTimeMillis();
-////        for(int i = 0 ; i < array2.length ; i++) {
-////            DeterministicLinearTimeSelection.linearTimeSelection(array2, i+1);
-////        }
-//        int linear = DeterministicLinearTimeSelection.linearTimeSelection(Arrays.copyOf(array2, array2.length), array2.length / 2);
-//        endTime = System.currentTimeMillis();
-//        System.out.println("Time taken by linear (" + linear + "): " + ((endTime - startTime)));
-//
-//        startTime = System.currentTimeMillis();
-////        for(int i = 0 ; i < array2.length ; i++) {
-////            SortingSelection.sortingSelection(array2, i+1);
-////        }
-//        int sorting = SortingSelection.sortingSelection(Arrays.copyOf(array2, array2.length), array2.length / 2);
-//        endTime = System.currentTimeMillis();
-//        System.out.println("Time taken by sorting (" + sorting + "): " +  ((endTime - startTime)));
-//    }
 
     public static void main(String[] args) {
         int[] sizes = new int[] {100, 1000, 10000, 100000, 200000, 500000, 900000, 1000000, 2000000, 5000000, 9000000, 10000000, 20000000};
